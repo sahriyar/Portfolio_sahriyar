7598b0b975c06ec097eca5727de2b4b901d57b59
sahriyar eshar <princeeshar@gmail.com>
2022-08-29 17:50:16 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ===================== Typing animation ===================== */
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","Web Developer","Software Developer","Frontend Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
