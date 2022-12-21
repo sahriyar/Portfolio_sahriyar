33a4481a4c6063303d9a7d9c07bbd437f957afb5
sahriyar eshar <princeeshar@gmail.com>
2022-12-21 17:09:50 +0600

    portfolio

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* ===================== Typing animation ===================== */
-var typed = new Typed(".typing", {
-    strings:["","Web Designer","Web Developer","Software Developer","Frontend Developer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
